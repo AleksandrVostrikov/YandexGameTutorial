180a6c73c549002e36d7bf6ee4dac3bf72a1d001
AleksandrVostrikov <vostrik-aleksandr@yandex.ru>
2022-11-07 22:57:43 +0300

    07.11

--- YandexGameTutorial/Assets/Scripts/PlayerMover.cs
+++ YandexGameTutorial/Assets/Scripts/PlayerMover.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
-    
+    [SerializeField] private float _speed;
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position += transform.forward * _speed * Time.fixedDeltaTime;
+        }
+    }
 }
