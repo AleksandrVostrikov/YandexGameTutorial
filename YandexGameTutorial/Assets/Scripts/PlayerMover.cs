242226a297f4ecaa5f6e64ddc05acd541d95a1f7
AleksandrVostrikov <vostrik-aleksandr@yandex.ru>
2022-11-08 22:28:01 +0300

    08.11

--- YandexGameTutorial/Assets/Scripts/PlayerMover.cs
+++ YandexGameTutorial/Assets/Scripts/PlayerMover.cs
@@ -16,22 +16,30 @@ public class PlayerMover : MonoBehaviour
 
     private void MovePlayer()
     {
+        if (Input.GetMouseButton(0))
+        {
+            SetPosition();
+            SetAngle();
+        }
+    }
+
+    private void SetPosition()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             _previousMousPosition = Input.mousePosition.x;
         }
 
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 position = transform.position += transform.forward * _speed * Time.deltaTime;
-            position.x = Mathf.Clamp(position.x, -2.5f, 2.5f);
-            transform.position = position;
-
-            float deltaX = Input.mousePosition.x - _previousMousPosition;
-            _previousMousPosition = Input.mousePosition.x;
-            _eulerAngleY += deltaX;
-            _eulerAngleY = Mathf.Clamp(_eulerAngleY, -70, 70);
-            transform.eulerAngles = new Vector3(0, _eulerAngleY, 0);
-        }
+        Vector3 position = transform.position += transform.forward * _speed * Time.deltaTime;
+        position.x = Mathf.Clamp(position.x, -2.5f, 2.5f);
+        transform.position = position;
+    }
+    private void SetAngle()
+    {
+        float deltaX = Input.mousePosition.x - _previousMousPosition;
+        _previousMousPosition = Input.mousePosition.x;
+        _eulerAngleY += deltaX;
+        _eulerAngleY = Mathf.Clamp(_eulerAngleY, -70, 70);
+        transform.eulerAngles = new Vector3(0, _eulerAngleY, 0);
     }
 }
